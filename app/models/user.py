77d1192b1166ff5d2ab445d05eb89cc4311017a5
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-06-20 20:33:13 -0400

    altered users table

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+import os
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -10,17 +10,22 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    profile_picture_url = db.Column(db.String(255))
+    dark_mode_enabled = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    @property
-    def password(self):
-        return self.hashed_password
-
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+    def __init__(self, username, email, password, profile_picture_url=None, dark_mode_enabled=False):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.profile_picture_url = profile_picture_url or os.path.join(
+            'assets/images', 'user.png')
+        self.dark_mode_enabled = dark_mode_enabled
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -29,5 +34,9 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'profile_picture_url': self.profile_picture_url,
+            'dark_mode_enabled': self.dark_mode_enabled,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
