346f7334f36a477b637791ddba95e905372025e5
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-06-21 19:17:38 -0400

    created goals, todos, reminders, dailyplanner and dailyplannerslots tables in db

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .goal import Goal
+from .to_do import Todo
+from .reminder import Reminder
+from .daily_planner import DailyPlanner
+from .daily_planner_slot import DailyPlannerSlot
 from .db import environment, SCHEMA
