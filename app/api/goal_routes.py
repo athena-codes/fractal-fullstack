0607a7b6970667133c8569f89271ea634367933f
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-06-24 09:46:43 -0400

    get a goal and update goal working

--- app/api/goal_routes.py
+++ app/api/goal_routes.py
@@ -9,6 +9,7 @@ goal_routes = Blueprint('goals', __name__)
 
 # Create new goal
 @goal_routes.route('/', methods=['POST'])
+@login_required
 def create_goal():
     if not current_user.is_authenticated:
         return jsonify(error='User not authenticated'), 401
@@ -25,6 +26,44 @@ def create_goal():
                 end_date=end_date, timeframe=timeframe)
 
     db.session.add(goal)
+    db.session.commit()
+
+    return jsonify(goal.to_dict()), 200
+
+
+# Get details of specific goal
+@goal_routes.route('/<goal_id>', methods=['GET'])
+@login_required
+def get_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if goal.user_id != current_user.id:
+       return jsonify({'message': 'Unauthorized'}), 401
+
+    if not goal:
+        return jsonify({'message': 'Goal not found'}), 404
+
+    return jsonify(goal.to_dict()), 200
+
+
+# Update a specific goal
+@goal_routes.route('/<goal_id>', methods=['PUT'])
+@login_required
+def update_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return jsonify({'message': 'Goal not found'}), 404
+
+    if goal.user_id != current_user.id:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    data = request.get_json()
+    goal.title = data.get('title')
+    goal.description = data.get('description')
+    goal.end_date = datetime.strptime(data.get('end_date'), '%Y-%m-%d')
+    goal.timeframe = data.get('timeframe')
+
     db.session.commit()
 
     return jsonify(goal.to_dict()), 200
