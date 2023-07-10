ed6d74bb7e855e43a56aa24f79712c9437579059
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-07-10 16:19:03 -0400

    added styling to goals overview and changed app font

--- react-app/src/components/Goals/GoalsOverviewPage/index.js
+++ react-app/src/components/Goals/GoalsOverviewPage/index.js
@@ -6,6 +6,8 @@ import { updateExistingGoal } from '../../../store/goals'
 import UpdateGoalModal from '../UpdateGoalModal'
 import OpenModalButton from '../../OpenModalButton'
 
+import './GoalsOverviewPage.css'
+
 const GoalsOverview = () => {
   const dispatch = useDispatch()
   const goals = useSelector(state => state.goals.goals)
@@ -36,45 +38,84 @@ const GoalsOverview = () => {
     }
   }
 
+  // FORMAT DATE
+  const getOrdinalSuffix = day => {
+    if (day >= 11 && day <= 13) {
+      return 'th'
+    }
+    switch (day % 10) {
+      case 1:
+        return 'st'
+      case 2:
+        return 'nd'
+      case 3:
+        return 'rd'
+      default:
+        return 'th'
+    }
+  }
+
+  const formatDate = dateString => {
+    const date = new Date(dateString)
+    const timeZoneOffset = date.getTimezoneOffset() * 60 * 1000 // Get the time zone offset in milliseconds
+    const adjustedDate = new Date(date.getTime() + timeZoneOffset) // Adjust the date by adding the time zone offset
+    const options = { weekday: 'long', month: 'long', day: 'numeric' }
+    const formattedDate = adjustedDate.toLocaleDateString('en-US', options)
+    const day = adjustedDate.getDate()
+    const suffix = getOrdinalSuffix(day)
+    return formattedDate.replace(/(\d+)$/, `$1${suffix}`)
+  }
+
   // LOADING SYMBOL
   if (!goals) {
     return <div>Loading...</div>
   }
 
   return (
-    <div>
+    <>
       <h2>Goals</h2>
+    <div className='goals-overview'>
       {goals.length === 0 ? (
         <p>No goals found.</p>
       ) : (
-        <ul>
+        <ul className='goals-list'>
           {goals.map(goal => (
-            <li key={goal.id}>
-              <h3>{goal.title}</h3>
-              <p>Description: {goal.description}</p>
-              <p>End Date: {goal.end_date}</p>
+            <li key={goal.id} className='goal-card'>
+              <h3 className='title'>{goal.title}</h3>
+              <p className='description'>{goal.description}</p>
+              <p>Deadline: {formatDate(goal.end_date)}</p>
               <p>Timeframe: {goal.timeframe} days</p>
-              <OpenModalButton
-                modalComponent={
-                  <UpdateGoalModal
-                    goalId={goal.id}
-                    title={goal.title}
-                    description={goal.description}
-                    endDate={goal.end_date}
-                    timeframe={goal.timeframe}
-                    onSubmit={handleUpdateGoal}
-                    onClose={() => setSelectedGoalId(null)}
+              <div className='goal-actions'>
+                <div className='modal-btn-goal'>
+                  <OpenModalButton
+                    modalComponent={
+                      <UpdateGoalModal
+                        goalId={goal.id}
+                        title={goal.title}
+                        description={goal.description}
+                        endDate={goal.end_date}
+                        timeframe={goal.timeframe}
+                        onSubmit={handleUpdateGoal}
+                        onClose={() => setSelectedGoalId(null)}
+                      />
+                    }
+                    buttonText='Update'
+                    onModalClose={() => setSelectedGoalId(null)}
                   />
-                }
-                buttonText='Update'
-                onModalClose={() => setSelectedGoalId(null)}
-              />
-              <button onClick={() => handleDeleteGoal(goal.id)}>Delete</button>
+                </div>
+                <button
+                  onClick={() => handleDeleteGoal(goal.id)}
+                  className='delete-button'
+                >
+                  Delete
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       )}
     </div>
+    </>
   )
 }
 
