5441967e315fb49d8c8a5e0c9d2b9f7dde73c6dd
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-08-09 11:03:29 -0400

    added NoteForm to the forms init file

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .create_goal_form import GoalForm
 from .create_todo_form import TodoForm
+from .create_note_form import NoteForm
