71fa47698681cd9e84f29fd85789acae1630d0e6
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-08-10 14:47:00 -0400

    create note working, styled form a bit added redirection to notes overview after submit

--- react-app/src/components/Notes/CreateNoteForm/index.js
+++ react-app/src/components/Notes/CreateNoteForm/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { createNewNote } from '../../../store/notes'
+
+import './CreateNoteForm.css'
 
 function CreateNoteForm () {
   const dispatch = useDispatch()
+  const history = useHistory()
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [errors, setErrors] = useState([])
@@ -20,13 +24,14 @@ function CreateNoteForm () {
     if (data && data.errors) {
       setErrors(data.errors)
     } else {
+      history.push('/notes')
       // Handle successful submission or redirection
     }
   }
 
   return (
     <div className='create-note-form'>
-      <h1>Create a New Note</h1>
+      <h1 className='new-note-heading'>Create a New Note</h1>
       <form onSubmit={handleSubmit}>
         <ul className='note-form-errors'>
           {errors.map((error, idx) => (
@@ -34,21 +39,23 @@ function CreateNoteForm () {
           ))}
         </ul>
         <label>
-          Title
           <input
+          className='title-input-notes'
             type='text'
             value={title}
             onChange={e => setTitle(e.target.value)}
+            placeholder='Note Title'
           />
         </label>
         <label>
-          Content
           <textarea
+          className = 'content-input-notes'
             value={content}
             onChange={e => setContent(e.target.value)}
+            placeholder='Write your note here!'
           />
         </label>
-        <button type='submit'>Create Note</button>
+        <button className='notes-submit-button' type='submit'>Submit</button>
       </form>
     </div>
   )
